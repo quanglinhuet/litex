bb86b4ef44f9a018220cbb83f08f82887a2d7513
quanglinhuet <quanglinh.uet@gmail.com>
2020-08-13 16:11:44 +0700

    .....

--- algorithm_b2.py
+++ algorithm_b2.py
@@ -5,8 +5,7 @@ def f(x,y):
         y=y//2 if y%2==0 else y+1
         count+=1
         s= str(y)+' '+ s
-        if y<x: 
-            count =count + x-y
-            for i in range(y+1,x+1): s= str(i)+' '+s
+    count =count + x-y
+    for i in range(y+1,x+1): s= str(i)+' '+s
     print('Step: '+ str(count)+'\n Way: '+s)
 f(3,51)
